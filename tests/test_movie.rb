23700e7651b8cb6acca9b6cf048f5fe273672ecb
Benjamin Guzman-Ayala <150963859+BenjaminGuzman-Ayala@users.noreply.github.com>
2024-02-29 17:09:47 +0000

    hhhklhklj

--- tests/test_movie.rb
+++ tests/test_movie.rb
@@ -4,6 +4,7 @@ require './movie_theater'
 class TestMovie < Minitest::Test
   def test_addition
     movie = MovieTheater.new
-    assert_equal "inception", " benji", movie.add_movie("inception", "benji"), "Add movie failed"
+    assert_equal "inception", "benji", movie.add_movie("inception", "benji"), "Add movie failed"
   end
 end
+
